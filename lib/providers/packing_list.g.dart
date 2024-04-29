236d0340f380dfadec4e736cbcd7962cd54a09f9
Jordan Corbman <corbmanj@gmail.com>
2024-04-28 20:35:11 -0700

    packing list providers

---
+++ lib/providers/packing_list.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'packing_list.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$packingListNotifierHash() =>
+    r'd7ce4b5c4ddc0f0cc17bc9a6ed580c5a62c7bbeb';
+
+/// See also [PackingListNotifier].
+@ProviderFor(PackingListNotifier)
+final packingListNotifierProvider =
+    AutoDisposeNotifierProvider<PackingListNotifier, PackingListType>.internal(
+  PackingListNotifier.new,
+  name: r'packingListNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$packingListNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PackingListNotifier = AutoDisposeNotifier<PackingListType>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
