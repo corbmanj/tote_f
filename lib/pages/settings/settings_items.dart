9e30dfbf15e79c76e03948ff4c9390ef8e7f1200
Jordan Corbman <corbmanj@gmail.com>
2025-05-25 09:10:08 -0600

    add split view to items list

--- lib/pages/settings/settings_items.dart
+++ lib/pages/settings/settings_items.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:split_view/split_view.dart';
 import 'package:tote_f/pages/settings/additional_item_chip.dart';
 import 'package:tote_f/pages/settings/item_chip.dart';
 import 'package:tote_f/providers/user_additional_items_provider.dart';
 import 'package:tote_f/providers/user_items_provider.dart';
-
 
 // TODO: split up the future provider and have the two parts of the page render separeately
 class CombinedResult {
@@ -34,66 +34,91 @@ class SettingsItems extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final itemsList = ref.watch(combinedProvider);
-    // final additionalItemsList = ref.watch(userAdditionalItemsProvider);
 
     return switch (itemsList) {
       AsyncData(value: final items) => Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                  style: TextStyle(fontWeight: FontWeight.bold), "Outfit Items")
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Optional: Add a page title here
+          Expanded(
+            child: SplitView(
+              viewMode: SplitViewMode.Vertical,
+              gripSize: 8,
               children: [
-                Text('Item'),
-                Text('Grouping'),
-                Text('Is Generic'),
-                Text('Actions'),
+                // Outfit Items Section
+                Column(
+                  children: [
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              "Outfit Items")
+                        ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('Item'),
+                        Text('Grouping'),
+                        Text('Is Generic'),
+                        Text('Actions'),
+                      ],
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(12.0),
+                        child: ListView.separated(
+                            scrollDirection: Axis.vertical,
+                            itemBuilder:
+                                (BuildContext context, int itemIndex) =>
+                                    ItemChip(
+                                        item: items.userItemsAndGroups
+                                            .userItems[itemIndex]),
+                            separatorBuilder:
+                                (BuildContext context, int outfitIndex) =>
+                                    SizedBox(width: 30.0),
+                            itemCount:
+                                items.userItemsAndGroups.userItems.length),
+                      ),
+                    ),
+                  ],
+                ),
+                // Additional Items Section
+                Column(
+                  children: [
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              "Additional Items")
+                        ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('Item'),
+                        Text('Actions'),
+                      ],
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(12.0),
+                        child: ListView.separated(
+                            scrollDirection: Axis.vertical,
+                            itemBuilder:
+                                (BuildContext context, int itemIndex) =>
+                                    AdditionalItemChip(
+                                        item: items
+                                            .userAdditionalItemsAndSections
+                                            .userAdditionalItems[itemIndex]),
+                            separatorBuilder:
+                                (BuildContext context, int outfitIndex) =>
+                                    SizedBox(height: 8.0),
+                            itemCount: items.userAdditionalItemsAndSections
+                                .userAdditionalItems.length),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                margin: EdgeInsets.all(12.0),
-                child: ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (BuildContext context, int itemIndex) =>
-                        ItemChip(item: items.userItemsAndGroups.userItems[itemIndex]),
-                    separatorBuilder: (BuildContext context, int outfitIndex) =>
-                        SizedBox(width: 30.0),
-                    itemCount: items.userItemsAndGroups.userItems.length),
-              ),
-            ),
-            const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  "Additional Items")
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Item'),
-                Text('Actions'),
-              ],
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: EdgeInsets.all(12.0),
-                child: ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (BuildContext context, int itemIndex) =>
-                        AdditionalItemChip(item: items.userAdditionalItemsAndSections.userAdditionalItems[itemIndex]),
-                    separatorBuilder: (BuildContext context, int outfitIndex) =>
-                        SizedBox(height: 8.0),
-                    itemCount: items.userAdditionalItemsAndSections.userAdditionalItems.length),
-              ),
-            )
-          ],
-        ),
-      _ => CircularProgressIndicator(),
-    };
-  }
-}
+          ),        ],      ),      _ => const Center(child: CircularProgressIndicator()),    };  }}
